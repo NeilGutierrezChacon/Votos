ea34efe681a62d8401a3c7a320ce73a17e901d3a
Ubuntu <a18neigutcha@inspedralbes.cat>
2020-04-08 22:34:24 +0200

    Pagina web desarrollada en Angular Cli

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Link } from './link/link.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Votos';
+
+  links: Link[];
+
+  constructor(){
+    this.links=[
+      new Link("Angular","https://angular.io",100),
+      new Link("Youtube","https://youtube.com",300),
+      new Link("FaztWeb","https://angular.io",100)
+
+    ]
+    console.log(this.links);
+  }
+
+  addLink(title: HTMLInputElement,link: HTMLInputElement){
+    this.links.push(new Link(title.value,link.value));
+    title.value='';
+    link.value='';
+    return false;
+  }
 }
